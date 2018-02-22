6194e42e68737e9fa01f03023667e94fb0385820
Lillian Lee <lzl003@ucsd.edu>
2018-02-22 10:36:26 -0800

    Google analytics

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(function(e) {
+		e.preventDefault();
+		ga('create','UA-114632773-1','auto');
+		ga("send","event",'like','click');
+	});
 }
+
